b397fcd34e78fc7d017895a0b1767a9e57cd13cb
ashrovy <ashrovy@gmail.com>
2019-05-30 16:35:39 +0700

    c

---
+++ spotify/app/background.js
@@ -0,0 +1,26 @@
+(function() {
+    chrome.app.runtime.onLaunched.addListener(start);
+    chrome.runtime.onInstalled.addListener(start);
+
+    function start() {
+        chrome.app.window.create('views/window.html',
+            {
+                'innerBounds': {
+                      'width': 400,
+                      'height': 665,
+                      'minWidth': 700,
+                      'minHeight': 600,
+                      'maxWidth': 300,
+                      'maxHeight': 600
+                },
+                id: 'whatsapp', frame : {'color' : '#091112'}
+            },
+            onWindowCreated
+        );
+    }
+
+    function onWindowCreated(appWin) {
+        //nothing for now 
+    }
+
+}());
